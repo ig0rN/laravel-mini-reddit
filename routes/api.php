90c4f41cf75e98eccd71c7517114a907e0cb1908
Igor Nikolic <igornikolic77@yahoo.com>
2019-10-19 18:02:47 +0200

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', 'Auth\RegisterController@register');
+
+Route::middleware('auth:api')->group(function(){
+
+    Route::get('/threads', 'ThreadController@index');
+    Route::post('/threads', 'ThreadController@store');
+    Route::get('/threads/{thread}', 'ThreadController@show');
+    Route::post('/threads/{thread}', 'ThreadController@reply');
+    Route::put('/threads/{thread}', 'ThreadController@update');
+    Route::delete('/threads/{thread}', 'ThreadController@destroy');
+
+    Route::get('/comments/{comment}', 'CommentController@show');
+    Route::post('/comments/{comment}', 'CommentController@reply');
+    Route::delete('/comments/{comment}', 'CommentController@destroy');
+
 });
